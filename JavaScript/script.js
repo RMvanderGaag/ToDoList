a5b07bb234c79a3b535c27198c460d89ac7782cf
Rogier van der Gaag <42837230+RMvanderGaag@users.noreply.github.com>
2020-04-08 11:24:51 +0200

    var name changes

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,22 +1,22 @@
 //Deze functie is voor de filter op de rijen
-function filterList(c){
+function filterList(status){
     //Alle rijen worden in de variabele 'x' gezet
-    var x = document.getElementsByClassName('tableRow');
+    var tablerows = document.getElementsByClassName('tableRow');
     //Alle rijen die dezelfde class hebben als de meegestuurde parameter worden in de variabele 'y' gezet 
-    var y = document.getElementsByClassName(c);
+    var statusRow = document.getElementsByClassName(status);
     //Hij loopt door alle rijen heen
-    for(var i = 0; i < x.length; i++){
-        if(c == 'all'){
+    for(var i = 0; i < tablerows.length; i++){
+        if(status == 'all'){
             //Als de parameter gelijk is aan 'all' dan word de display 'table-row' 
-            x[i].style.display = 'table-row';
+            tablerows[i].style.display = 'table-row';
         }else{
             //Anders word bij elke rij de display op 'none' gezet 
-            x[i].style.display = 'none';
+            tablerows[i].style.display = 'none';
         }
     }
 
     //Nu word er door alle rijen heen geloopt die gelijk zijn aan 'y' er word bij elke rij dispay 'table-row' toegevoegd  
-    for(var j = 0; j < y.length; j++){
-        y[j].style.display = 'table-row'
+    for(var j = 0; j < statusRow.length; j++){
+        statusRow[j].style.display = 'table-row'
     }
 }
